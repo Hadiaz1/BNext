a854311ab0bd6a39d3cbcb175812099f9cc4e3bf
hadiaz1 <hadialzein123@gmail.com>
2023-07-26 14:17:15 +0200

    test

--- src/train_assistant_group_amp.py
+++ src/train_assistant_group_amp.py
@@ -824,13 +824,13 @@ def main():
     if not os.path.exists('log'):
         os.mkdir('log')
 
+
     log_format = '%(asctime)s %(message)s'
     logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                         format=log_format, datefmt='%m/%d %I:%M:%S %p')
     fh = logging.FileHandler(os.path.join('log/log.txt'))
     fh.setFormatter(logging.Formatter(log_format))
     logging.getLogger().addHandler(fh)
-
 
     if args.seed is not None:
         random.seed(args.seed)
